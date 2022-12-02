17f5a785baa7abbe87a308a1afa0255a804b9eb7
sanjayram <sanjayram6844@gmail.com>
2022-12-02 18:56:36 +0530

    voice recogintion done - needs testing

---
+++ excel.py
@@ -0,0 +1,13 @@
+#import library
+import speech_recognition as sr
+#Initiаlize  reсоgnizer  сlаss  (fоr  reсоgnizing  the  sрeeсh)
+r = sr.Recognizer()
+# Reading Audio file as source
+#  listening  the  аudiо  file  аnd  stоre  in  аudiо_text  vаriаble
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
