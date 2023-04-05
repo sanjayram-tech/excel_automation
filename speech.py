2e829a9b35c9648b6409b93acdf4eeccad98d318
Sanjayram <sujatha6844@gmail.com>
2023-04-05 17:06:35 +0530

    need to update the dataset and create a new model

--- speech.py
+++ speech.py
@@ -10,9 +10,10 @@ wb = Workbook()
 wb = load_workbook("sample.xlsx")
 ws = wb.active
 
-a=""
-while(True):
-    break
+a="multiply a1 and a2"
+ch ='y'
+while(ch=='y'):
+    ##break
     r = sr.Recognizer()
     with sr.Microphone() as source: 
         print("Listening ...")
@@ -23,8 +24,9 @@ while(True):
             query = r.recognize_google(audio, language = 'en-IN', show_all=True)
             print(query['alternative'][0]['transcript'])
             a=  query['alternative'][0]['transcript'].lower()
-            if a.__contains__("stop"):
-                break
+            ch=input("Do you want to continue y/n")
+            # if a.__contains__("stop"):
+            #     break
         # finally:
         #     pass
         except Exception as e:
@@ -39,27 +41,46 @@ operation = []
 cell = []
 
 nlp = spacy.load("content\model-best") 
-transcriptions = "add a1 b1 store in c3"
+transcriptions = a
 doc = nlp(transcriptions)
 print(transcriptions)
 print(doc.ents)
 matcher = Matcher(nlp.vocab)
-pattern = [{"ENT_TYPE": "CELL"}]
-#print(pattern[0]["ENT_TYPE"])
-matcher.add("", [pattern])
+pattern = [{"ENT_TYPE": "OPERATION"}]
+matcher.add("", [pattern]) 
 matches = matcher(doc)
 for match_id, start, end in matches:
     string_id = nlp.vocab.strings[match_id]  # Get string representation
     span = doc[start:end]  # The matched span
-    if pattern[0]["ENT_TYPE"]=="CELL":
-        cell.append(span.text.upper())
     if pattern[0]["ENT_TYPE"]=="OPERATION":
         operation.append(span.text)
-    print( span.text)
+        print(span.text)
+print (operation)
+# ###########################################################################
+mat = Matcher(nlp.vocab)
+patt = [{"ENT_TYPE": "CELL"}]
+mat.add("", [patt]) 
+match = mat(doc)
+for match_id, start, end in match:
+    string_id = nlp.vocab.strings[match_id]  # Get string representation
+    span = doc[start:end]  # The matched span
+    if patt[0]["ENT_TYPE"]=="CELL":
+        cell.append(span.text.upper())
+        print(span.text)
+print("in cell")
+print(cell)
 if operation[0]=="add":
-    ws[cell[2].value]=ws[cell[0].value]+ws[cell[1]]
+    ws["A3"].value=ws[cell[0]].value+ws[cell[1]].value
     wb.save("sample.xlsx")
-
+if operation[0]=="subract":
+    ws["A3"].value=ws[cell[0]].value-ws[cell[1]].value
+    wb.save("sample.xlsx")
+if operation[0]=="multiply":
+    ws["A3"].value=ws[cell[0]].value*ws[cell[1]].value
+    wb.save("sample.xlsx")
+if operation[0]=="divide":
+    ws["A3"].value=ws[cell[0]].value/ws[cell[1]].value
+    wb.save("sample.xlsx")
 
     
 #spacy.displacy.serve(doc, style='ent')
